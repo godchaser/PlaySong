65f549432bfce3347830538247258b507c8949fb
Samuel Koprivnjak <samuel.koprivnjak@gmail.com>
2015-06-27 18:11:12 +0200

    Sanitize first empty line

--- app/models/helpers/XMLSongsParser.java
+++ app/models/helpers/XMLSongsParser.java
@@ -24,7 +24,8 @@ public class XMLSongsParser {
 	public static void convertToSong(SongsXML songs) {
 		System.out.println("Checking if unmarshalled XML songs is null");
 		if (songs.getSongs() != null) {
-			System.out.println("Now converting unmarshalled XML songs to Song object: ");
+			System.out
+					.println("Now converting unmarshalled XML songs to Song object: ");
 			for (SongXML songXML : songs.getSongs()) {
 				Song song = new Song();
 				song.setId(Long.parseLong(songXML.getId()));
@@ -37,7 +38,27 @@ public class XMLSongsParser {
 				for (SongLyricsXML lyricsXML : songXML.getLyrics()) {
 					SongLyrics lyrics = new SongLyrics();
 					lyrics.setId(Long.parseLong(lyricsXML.getId()));
-					lyrics.setsongLyrics(lyricsXML.getSongLyrics());
+
+					String[] lines = lyricsXML.getSongLyrics().split("\n");
+					String sanitizedLyrics = null;
+					// Check if first line is empty, and then delete it
+					if (lines[0].length() < 2) {
+						System.out.println("Sanitizing leading blank line");
+						StringBuilder sb = new StringBuilder();
+						boolean skippedFirstLine = false;
+						for (String line : lines) {
+							if (skippedFirstLine) {
+								sb.append(line + "\n");
+							} else {
+								skippedFirstLine = true;
+							}
+						}
+						sanitizedLyrics = sb.toString();
+					} else {
+						sanitizedLyrics = lyricsXML.getSongLyrics();
+					}
+
+					lyrics.setsongLyrics(sanitizedLyrics);
 					String songKey = LineTypeChecker.getSongKey(lyricsXML
 							.getSongLyrics());
 					lyrics.setSongKey(songKey);
@@ -47,7 +68,8 @@ public class XMLSongsParser {
 				Song.updateOrCreateSong(song);
 			}
 		} else {
-			System.out.println("Skipping conversions - unmarshalled songs are null");
+			System.out
+					.println("Skipping conversions - unmarshalled songs are null");
 		}
 	}
 
