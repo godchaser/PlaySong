66f96d07ee850d71bd38ec936e73d6a5909cccaa
Samuel Koprivnjak <samuel.koprivnjak@gmail.com>
2015-12-27 20:52:23 +0100

    Fix unused html imports, metis menu commented

--- public/javascripts/sb-admin-2.js
+++ public/javascripts/sb-admin-2.js
@@ -1,10 +1,9 @@
-/*
 $(function() {
 
-    $('#side-menu').metisMenu();
+    //$('#side-menu').metisMenu();
 
 });
-*/
+
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
